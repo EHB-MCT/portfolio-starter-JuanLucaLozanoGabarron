15b26f4c1be977d8ea4493181eb13d59faebfc08
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2023-10-12 10:23:33 +0200

    Initialize setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const { error } = require("console");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = 3000;
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(port, (err) => {
+  if (!err) {
+    console.log("running on port " + port);
+  } else {
+    console.error(err);
+  }
+});
